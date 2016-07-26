d2c2da830ef232600883fa279239b154b2071c4d
RamblinRoyce <ChingRoyce@gmail.com>
2016-07-25 19:37:10 -0500

    update connection.js jawsdb

--- app/config/connection.js
+++ app/config/connection.js
@@ -11,18 +11,23 @@ var source = {
         port: 3306,
         host: 'localhost',
         user: 'root',
-        password: "Njmitx123",
+        password: "tsingta0",
         database: "starwars"
     },
     jawsDB: {
-
+        port: 3306,
+        host: 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'axf2n6w6q8nbaraj',
+        password: "lc8l6wh5z00ra9r8",
+        database: "j859072eshddecwr"
+        //  "mysql://axf2n6w6q8nbaraj:lc8l6wh5z00ra9r8@nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j859072eshddecwr"
         // TODO: Add your JawsDB connection info here
     }
 }
 
 
 // TODO: connect your jawsDB connection witht he mysql connection.
-var connection = mysql.createConnection( //enter connection here);
+var connection = mysql.createConnection("mysql://axf2n6w6q8nbaraj:lc8l6wh5z00ra9r8@nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j859072eshddecwr"); //enter connection here);
 
 
 
